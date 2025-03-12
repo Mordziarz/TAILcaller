6298137a35f651bee6096936c7be373238f3964f
Mordziarz <139069437+Mordziarz@users.noreply.github.com>
2025-03-12 12:34:25 +0100

    Update get_gene_id.R

--- R/get_gene_id.R
+++ R/get_gene_id.R
@@ -1,11 +1,13 @@
 #' Merging a polyA table with gene_id from gtf file
 #'
 #' @param get_polyA_out the table was created using the get_polyA_out function
+#' @param gtf_file the gtf file
 #' @param transcript_column_gtf the name of the column that contains the transcript_id
+#' @param gene_column_gtf the name of the column that contains the gene_id
 #' @return A table object.
 #' @export
 #'
-get_gene_id <- function(polyA_table=get_polyA_out,gtf_file=gtf,transcript_column_gtf="column_name") {
+get_gene_id <- function(polyA_table=get_polyA_out,gtf_file= gtf, transcript_column_gtf="column_name",gene_column_gtf="column_name") {
   
   if (!base::is.data.frame(polyA_table)) {
     stop("Argument 'polyA_table' must be a data frame.")
@@ -15,9 +17,19 @@ get_gene_id <- function(polyA_table=get_polyA_out,gtf_file=gtf,transcript_column
     stop(paste("Argument 'transcript_column_gtf' must be a column name in the gtf data frame. Please check if the column", transcript_column_gtf, "exists."))
   }
   
+    if (!base::is.character(gene_column_gtf) || !(gene_column_gtf %in% colnames(gtf_file))) {
+    stop(paste("Argument 'gene_column_gtf' must be a column name in the gtf data frame. Please check if the column", gene_column_gtf, "exists."))
+  }
+
   base::message("Starting to process the data...")
   
   start_time <- base::Sys.time()
+
+  gtf <- base::as.data.frame(gtf_file)
+
+  gtf <- gtf[!base::duplicated(gtf$transcript_column_gtf),]
+  
+  gtf <- gtf[,c(gene_column_gtf,transcript_column_gtf)]
 
   get_gene_id_out <- base::merge(polyA_table,gtf_file,by.x="transcript_id",by.y=transcript_column_gtf)
   
