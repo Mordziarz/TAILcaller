36b386e16e7245b409b880fc2257af34686dc854
Mordziarz <139069437+Mordziarz@users.noreply.github.com>
2025-03-12 12:02:43 +0100

    Update get_polyA.R

--- R/get_polyA.R
+++ R/get_polyA.R
@@ -17,35 +17,35 @@ get_polyA <- function(samples_table=samples_table) {
 
   param <- Rsamtools::ScanBamParam(what = c("qname", "rname"),tag="pt")
 
-  tabela_ogony_bam <- base::data.frame()
+  table_ogony_bam <- base::data.frame()
 
   for (i in 1:base::nrow(samples_table)) {
 
     bam_data <- Rsamtools::scanBam(samples_table$bam_path[i],param=param)
 
-    tabela_bam <- base::as.data.frame(bam_data)
+    table_bam <- base::as.data.frame(bam_data)
 
-    tabela_bam$samples_name <- samples_table$sample_name[i]
+    table_bam$samples_name <- samples_table$sample_name[i]
 
-    tabela_bam$group <- samples_table$group[i]
+    table_bam$group <- samples_table$group[i]
 
-    tabela_ogony_bam <- base::rbind(tabela_ogony_bam,tabela_bam)
+    table_ogony_bam <- base::rbind(table_ogony_bam,table_bam)
 
     if (i %% 1 == 0) {
       message(paste("Processed samples:", i, "out of", base::nrow(samples_table)))
     }
   }
 
-  colnames(tabela_ogony_bam) <- c("read_id", "transcript_id", "polyA_length", "sample_name", "group")
+  colnames(table_ogony_bam) <- c("read_id", "transcript_id", "polyA_length", "sample_name", "group")
 
-  tabela_ogony_bam <- tabela_ogony_bam[!is.na(tabela_ogony_bam$polyA_length),]
+  table_ogony_bam <- table_ogony_bam[!is.na(table_ogony_bam$polyA_length),]
 
-  tabela_ogony_bam <- tabela_ogony_bam[!is.na(tabela_ogony_bam$transcript_id),]
+  table_ogony_bam <- table_ogony_bam[!is.na(table_ogony_bam$transcript_id),]
 
   end_time <- base::Sys.time()
 
   base::message("Execution time: ", base::round(difftime(end_time, start_time, units = "mins"),2), " minutes")
 
-  return(tabela_ogony_bam)
+  return(table_ogony_bam)
 
 }
