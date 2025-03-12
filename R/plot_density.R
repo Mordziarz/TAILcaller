4c91527925be04e75400b913bb662f91e34638f7
Mordziarz <139069437+Mordziarz@users.noreply.github.com>
2025-03-12 13:28:57 +0100

    Update plot_density.R

--- R/plot_density.R
+++ R/plot_density.R
@@ -54,16 +54,20 @@ density_plot <- density_plot + ggplot2::geom_vline(data = means, ggplot2::aes(xi
 
 formula <- base::paste0("polyA_length ~ ", grouping_column)
 
-if(nrow(unique(polyA_table$grouping_column)) == 1) {
-res_stats <- "The Wilcoxon or Kruskal-Wallis test could not be performed because your n = 1."
-}
-
-if(nrow(unique(polyA_table$grouping_column)) == 2) {
-res_stats <- stats::wilcox.test(stats::as.formula(formula), data=polyA_table)
-}
-
-if(nrow(unique(polyA_table$grouping_column)) > 2) {
-res_stats <- kruskal.test(Sepal_Width~Species, data=iris_sub)
+if (grouping_column %in% names(polyA_table)) {
+  unique_count <- nrow(unique(polyA_table[grouping_column]))
+}  
+  if (unique_count == 1) {
+    res_stats <- "The Wilcoxon or Kruskal-Wallis test could not be performed because your n = 1."
+  } else if (unique_count == 2) {
+    res_stats <- stats::wilcox.test(formula, data = polyA_table)
+  } else if (unique_count > 2) {
+    res_stats <- kruskal.test(formula, data = polyA_table)
+  } else {
+    res_stats <- "Unexpected number of unique values in grouping column."
+  }
+} else {
+  res_stats <- paste("Error: Column", grouping_column, "not found in polyA_table.")
 }
 
 return(list(wilcox_test = res_stats, plot = density_plot))
