0819e821c8e652a74c8dfa3b451b09f09ae3831c
Mordziarz <139069437+Mordziarz@users.noreply.github.com>
2025-03-12 14:05:53 +0100

    Create calculate_kruskal_polyA.R

---
+++ R/calculate_kruskal_polyA.R
@@ -0,0 +1,57 @@
+#' Calculation of statistics and creation of the table
+#'
+#' @param polyA_table the table created using the get_polyA function.
+#' @param grouping_factor the name of the column in the table that divides the experiment into groups.
+#' @param which_level the name of the column by which the statistics should be grouped, either transcripts or genes.
+#' @return a table object.
+#' @export
+#'
+
+calculate_kruskal_polyA <- function(polyA_table=get_gene_id_out, grouping_factor="group", which_level="gene_id") {
+  
+  if (missing(polyA_table)) {
+    stop("'polyA_table' must be defined.")
+  }
+  
+  if (missing(control_group) | missing(treated_group)) {
+    stop("Both 'control_group' and 'treated_group' must be provided.")
+  }
+  
+  base::message("Starting to process the data and calculate statistics...")
+  
+  start_time <- base::Sys.time()
+  
+  how_molecules <- polyA_table[!duplicated(polyA_table[[which_level]]), ]
+  how_molecules <- how_molecules[, which_level, drop=FALSE]
+  
+  p_values <- base::numeric(base::nrow(how_molecules))
+  
+  formula <- base::paste0("polyA_length ~ ", grouping_factor)
+  
+  for (i in 1:base::nrow(how_molecules)) {
+    
+    which_molecule <- how_molecules[[which_level]][i]
+    
+    polyA_table_unique <- polyA_table[polyA_table[[which_level]] == which_molecule, ]
+    
+    if (base::nrow(base::unique(polyA_table_unique[grouping_factor])) > 2) {
+      
+      p_val <- base::suppressWarnings(stats::kruskal.test(stats::as.formula(formula), data = polyA_table_unique)$p.value)
+      p_values[i] <- p_val
+      
+      
+    } else {
+      p_values[i] <- NA
+    }
+  }
+  
+  how_molecules$p_value <- p_values
+  how_molecules$padj <- stats::p.adjust(how_molecules$p_value, method = "fdr")
+  
+  end_time <- base::Sys.time()
+  
+  base::message("Processing complete. Time taken: ", base::round(difftime(end_time, start_time, units = "mins"), 2), " minutes")
+  base::message("Statistics have been calculated successfully.")
+  
+  return(how_molecules)
+}
